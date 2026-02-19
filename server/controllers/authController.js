ed5321622ac07243f6e9409a4f31161699a427ca
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-02-19 21:50:42 +0530

    addserver/controllers/

---
+++ server/controllers/authController.js
@@ -0,0 +1,94 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel';
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.json({ success: false, message: 'Missing Details' })
+
+    }
+    try {
+
+        const existingUser = await userModel.findOne({ email })
+        if (existingUser) {
+            return res.json({ success: false, message: 'User already exsits' })
+
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expireIn: '7d' })
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+                'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true });
+    }
+    catch (eror) {
+        res.json({ success: false, message: error.message })
+    }
+}
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ success: false, message: 'Email and Password are required' })
+
+    }
+    try {
+
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: 'invalid Email' })
+
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({ success: false, message: 'invalid Password' })
+
+
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expireIn: '7d' })
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+                'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true });
+
+
+    }
+    catch (eror) {
+        res.json({ success: false, message: error.message })
+    }
+
+}
+
+export const logout = async (req, res) => {
+
+    try {
+
+        res.ClearCokkie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+                'none' : 'strict'
+
+        });
+
+        return res.json({ success: true, message: "Logged Out" });
+    }
+    catch (eror) {
+        res.json({ success: false, message: eror.message })
+    }
+}
