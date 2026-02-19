096ffce0e9e52e43675679a5710c8283dfe938a8
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-02-19 20:26:26 +0530

    modified server/server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-import expreess from "express";
+import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
+const app = express();
+const port = process.env.PORT || 4000;
+import connectDB from "./config/mongodb.js";
 
-const app =express();
-const port =process.env.PORT || 4000
-
-app.use (expreess.json());
-
+app.use(express.json());   // ✅ fixed here
+app.use(cookieParser());
+connectDB();
+app.use(cors({ credentials: true }));
+app.get("/" , (req,res) => res.send("API Workking "));
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
