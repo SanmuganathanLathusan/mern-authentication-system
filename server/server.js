70dd6e9053500239780f4121204bf7f2a613e25c
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-02-21 21:26:01 +0530

    modified  server/server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,5 +12,8 @@ app.use(express.json());   // ✅ fixed here
 app.use(cookieParser());
 connectDB();
 app.use(cors({ credentials: true }));
+
+//Api End point
 app.get("/" , (req,res) => res.send("API Workking "));
+app.use('/api/auth',authRouter)
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
